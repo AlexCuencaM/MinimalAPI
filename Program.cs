1e5d91977066f0b7e3c5f376de0f0d7cc56ee253
Alex Cuenca M <maxpowerxd8y9@gmail.com>
2023-06-21 22:57:52 -0500

    Starting to commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-using BackgroundQueueService;
 using Store;
 var builder = WebApplication.CreateBuilder(args);
 var videoConnection = builder.Configuration["Videos:StorageConnection"];
@@ -25,64 +24,26 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-//for small files
-app.MapPost("/videos", async (HttpRequest res) =>
-{
-    byte[] fileBytes;
-    string? Id = res.Form["Id"];
-    IFormFile? file = res.Form.Files
-        .FirstOrDefault(f => f.Length > 0 && f.Name.Contains("Video"));
-
-
-    if(string.IsNullOrEmpty(Id)){
-        return Results.BadRequest("Id field is required");
-    }
-    if(file is null){
-        return Results.BadRequest("Video field is required");
-    }
-    // await using Stream stream = file!.();
-
-    using(MemoryStream memoryStream = new()){
-        await file!.CopyToAsync(memoryStream);
-        fileBytes = memoryStream.ToArray();
-    }
-    return Results.Ok($"Video recorded successfully");
-})
-.WithName("CreateVideo")
-.WithOpenApi();
-app.MapPost("/v2/queue", async (HttpRequest req) =>
-{
-    MyStorageQueue myFirstQueue = new("my-first-queue",videoConnection ?? string.Empty);
-    await myFirstQueue.CreateQueue();
-    await myFirstQueue.InsertMessage("Hi there :)");
-    string result = await myFirstQueue.PeekMessage();
-    return Results.Ok(result);
-})
-.WithName("CreateQueue")
-.WithOpenApi();
-
 app.MapPost("/v2/Videos", async (HttpRequest req) =>
 {
     try{
         var form = await req.ReadFormAsync();
         string? Id = form["Id"];
         IFormFile? file = form.Files["Video"];
-        
-        // return Results.Ok(file?.ContentDisposition);
-        // form.Files
-        //     .FirstOrDefault(f => f.Length > 0 && f.Name.Contains("Video"));
         if(string.IsNullOrEmpty(Id)){
             return Results.BadRequest("Id field is required");
         }
         if(file is null){
             return Results.BadRequest("Video field is required");
         }
-        BlobStore myNewBlobStorage = new(videoConnection ?? string.Empty, new UploadFile(file.FileName));
+        BlobStore myNewBlobStorage = 
+            new(videoConnection ?? string.Empty, new UploadFile(file.FileName));
         await myNewBlobStorage.CreateContainerClient();
-
         var response = await myNewBlobStorage.UploadBlob(file);
-
         return Results.Ok(response.Value);
+    }
+    catch(Azure.RequestFailedException e){
+        return Results.BadRequest(e.Message);
     }
     catch(Exception e){
         return Results.BadRequest(e.Message);
